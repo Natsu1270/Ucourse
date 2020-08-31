d275c8a420c349b72805877e02a4656358ae9aaa
Duy Hùng <37929010+Natsu1270@users.noreply.github.com>
2020-09-01 00:57:16 +0700

    enhance my certificates

--- frontend/src/pages/MyCertificatePage/my-certificates.page.jsx
+++ frontend/src/pages/MyCertificatePage/my-certificates.page.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { getAllMyCoursesAndProgramsAPI } from '../../api/home.services'
-import { message, Skeleton, Timeline, Tree, Tabs, Table, Tag, Button, List } from 'antd'
+import { message, Skeleton, Timeline, Tree, Tabs, Table, Tag, Button, List, Avatar, Space } from 'antd'
 import { dayDiff, formatDate } from '../../utils/text.utils'
 import { useSelector } from 'react-redux'
 import { useHistory, Link } from 'react-router-dom'
 import { tokenSelector } from '../../redux/Auth/auth.selects'
-import {getAllCourseCertificate, getStudentCertificate} from '../../api/summary.services'
-import { DownOutlined } from '@ant-design/icons'
+import { getAllCourseCertificate, getStudentCertificate } from '../../api/summary.services'
+import { DownOutlined, ExpandOutlined } from '@ant-design/icons'
 import Constants from '../../constants'
+
+import certificateIcon from '../../assets/certificate.png'
+
 const { TabPane } = Tabs
 
-const MyCertificatePage = ({certificates, isLoading }) => {
+const MyCertificatePage = ({ certificates, isLoading }) => {
     const history = useHistory()
     const [loading, setLoading] = useState(false)
     const [mycertifData, setCertificates] = useState([])
@@ -29,7 +32,7 @@ const MyCertificatePage = ({certificates, isLoading }) => {
                     date: certificate.received_date,
                     file: certificate.file
                 })));
-               
+
             }
         }
         catch (err) {
@@ -42,7 +45,7 @@ const MyCertificatePage = ({certificates, isLoading }) => {
 
     useEffect(() => {
         getStudentCertificates();
-        
+
     }, [certificates])
 
 
@@ -58,21 +61,24 @@ const MyCertificatePage = ({certificates, isLoading }) => {
             title: 'Tên khóa học',
             dataIndex: 'name',
             key: 'name',
-            render: name => <span>{name}</span>
+            render: name => <Tag color="geekblue" style={{ fontSize: '1.8rem', padding: '0.5rem' }}>{name}</Tag>
         },
         {
             title: 'Ngày hoàn thành',
             dataIndex: 'date',
             key: 'date',
-            render: date => <span>{formatDate(date, Constants.MMM_Do__YY__TIME)}</span>
+            render: date => <span>{formatDate(date, Constants.MMM_Do_YYYY)}</span>
         },
         {
             title: 'File chứng chỉ',
             dataIndex: 'file',
             key: 'file',
-            render: file => <span>{file}</span>
+            render: file => <Space onClick={() => window.open(file)}>
+                <Avatar size={48} shape="square" src={certificateIcon} />
+                <Button><ExpandOutlined /> Click để xem</Button>
+            </Space>
         },
-       
+
 
     ];
 
@@ -81,11 +87,11 @@ const MyCertificatePage = ({certificates, isLoading }) => {
         <section className="section-10 page ">
             <div className="page-card">
                 <h2 className="title--big text-center">Chứng chỉ của tôi</h2>
-            <Tabs defaultActiveKey="1">
-                <TabPane tab="Danh sách chứng chỉ" key="">
-                    <Table dataSource={mycertifData} columns={columns} />
-                </TabPane>
-            </Tabs>
+                <Tabs defaultActiveKey="1">
+                    <TabPane tab="Danh sách chứng chỉ" key="">
+                        <Table loading={loading} dataSource={mycertifData} columns={columns} />
+                    </TabPane>
+                </Tabs>
             </div>
         </section>
     )
